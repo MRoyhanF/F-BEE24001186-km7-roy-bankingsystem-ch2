5037773ef8ee91f6c586d16a3f7559ff4e139724
MRoyhanF <froyhan0@gmail.com>
2024-10-18 11:38:35 +0700

    feat: account | generate api get account by id

--- src/server/controllers/accountController.js
+++ src/server/controllers/accountController.js
@@ -15,6 +15,16 @@ class AccountController {
       next(new ErrorHandler(500, error.message));
     }
   }
+
+  async getAccountById(req, res, next) {
+    try {
+      const account = await this.accountService.getAccountById(req.params.id);
+      if (!account) throw new ErrorHandler(404, "User Not Found");
+      res.status(200).json({ Status: "Success", Data: account });
+    } catch (error) {
+      next(new ErrorHandler(500, error.message));
+    }
+  }
 }
 
 export default new AccountController();
--- src/server/routes/accountRoutes.js
+++ src/server/routes/accountRoutes.js
@@ -4,5 +4,6 @@ import AccountController from "../controllers/accountController.js";
 const router = express.Router();
 
 router.get("/", AccountController.getAllAccount.bind(AccountController));
+router.get("/:id", AccountController.getAccountById.bind(AccountController));
 
 export default router;
