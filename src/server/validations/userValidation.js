e30ba3483071f4a9f369afa74166a2b42c9c631c
MRoyhanF <froyhan0@gmail.com>
2024-10-18 22:33:31 +0700

    udpate: validation | user and account validation

--- src/server/validations/userValidation.js
+++ src/server/validations/userValidation.js
@@ -2,23 +2,23 @@ import Joi from "joi";
 
 export class UserValidation {
   static createUserSchema = Joi.object({
-    name: Joi.string().min(3).required(),
+    name: Joi.string().min(3).max(50).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
     profile: Joi.object({
       identity_type: Joi.string().valid("KTP", "SIM", "Passport").required(),
-      identity_number: Joi.string().min(16).required(),
+      identity_number: Joi.string().min(16).max(20).required(),
       address: Joi.string().min(3).required(),
     }).required(),
   });
 
   static updateUserSchema = Joi.object({
-    name: Joi.string().min(3),
+    name: Joi.string().min(3).max(50),
     email: Joi.string().email(),
     password: Joi.string().min(6),
     profile: Joi.object({
       identity_type: Joi.string().valid("KTP", "SIM", "Passport"),
-      identity_number: Joi.string().min(16),
+      identity_number: Joi.string().min(16).max(20),
       address: Joi.string().min(3),
     }),
   });
