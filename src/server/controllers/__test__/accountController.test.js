8afd049437bbdc6e34de724710266255239b1b61
MRoyhanF <froyhan0@gmail.com>
2024-10-26 22:18:30 +0700

    feat: testing | account update controller testing

--- src/server/controllers/__test__/accountController.test.js
+++ src/server/controllers/__test__/accountController.test.js
@@ -187,4 +187,33 @@ describe("AccountController", () => {
       expect(receivedError).toBeInstanceOf(ErrorHandler);
     });
   });
+
+  describe("updateAccount", () => {
+    it("should update account", async () => {
+      const account = { id: 1, account_number: 1234567890 };
+      AccountService.prototype.getAccountById.mockResolvedValue(account);
+      AccountService.prototype.getAccountByUser.mockResolvedValue(null);
+      AccountService.prototype.updateAccount.mockResolvedValue(account);
+
+      await AccountController.updateAccount(req, res, next);
+
+      expect(AccountService.prototype.getAccountById).toHaveBeenCalledTimes(1);
+      expect(AccountService.prototype.getAccountByUser).toHaveBeenCalledTimes(1);
+      expect(AccountService.prototype.updateAccount).toHaveBeenCalledTimes(1);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({ Status: "Success", Message: "Account Updated Successfully", Data: account });
+    });
+
+    it("should handle error", async () => {
+      const error = new Error("Internal server error");
+      AccountService.prototype.getAccountById.mockRejectedValue(error);
+
+      await AccountController.updateAccount(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+      const receivedError = next.mock.calls[0][0];
+
+      expect(receivedError).toBeInstanceOf(ErrorHandler);
+    });
+  });
 });
