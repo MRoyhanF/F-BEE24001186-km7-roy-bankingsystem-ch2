204296b90fc2104a3df382990513e54b6d91667f
MRoyhanF <froyhan0@gmail.com>
2024-10-28 12:44:09 +0700

    feat: unit testing | account validation testing

--- src/server/validations/__test__/accountValidation.test.js
+++ src/server/validations/__test__/accountValidation.test.js
@@ -1,30 +1,3 @@
-// import Joi from "joi";
-
-// export class AccountValidation {
-//   static createAccountSchema = Joi.object({
-//     bank_name: Joi.string().min(3).max(50).required(),
-//     bank_account_number: Joi.string().min(16).max(50).required(),
-//     balance: Joi.number().required(),
-//     user_id: Joi.number().required(),
-//   });
-
-//   static updateAccountSchema = Joi.object({
-//     bank_name: Joi.string().min(3).max(50),
-//     bank_account_number: Joi.string().min(16).max(50),
-//     balance: Joi.number(),
-//     user_id: Joi.number(),
-//   });
-
-//   static validate(schema, data) {
-//     const { error } = schema.validate(data);
-//     if (error) {
-//       throw new Error(error.details[0].message);
-//     }
-//   }
-// }
-
-// unit testing the AccountValidation class methods using Jest with mock data
-import Joi from "joi";
 import { AccountValidation } from "../accountValidation";
 
 describe("AccountValidation", () => {
