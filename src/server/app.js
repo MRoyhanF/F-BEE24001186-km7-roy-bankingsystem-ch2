d647a8b0fb9ab055d88c7a9c9a91d7ede62d71a8
MRoyhanF <froyhan0@gmail.com>
2024-10-29 22:04:12 +0700

    perf: route | update endpoint

--- src/server/app.js
+++ src/server/app.js
@@ -21,10 +21,10 @@ app.set("view engine", "ejs");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // API Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/accounts", accountRoutes);
-app.use("/api/transactions", transactionRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/accounts", accountRoutes);
+app.use("/api/v1/transactions", transactionRoutes);
 
 // Routes Not Found
 app.use(handleError);
