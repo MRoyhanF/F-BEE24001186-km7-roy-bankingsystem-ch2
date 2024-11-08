60c89dec52e5be1fe11abe48ccad9b468971b0c2
MRoyhanF <froyhan0@gmail.com>
2024-11-08 21:25:09 +0700

    test: test add route

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./docs/swagger2.json" with { type: "json" };
 
+import UserController from "./controllers/userController.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import accountRoutes from "./routes/accountRoutes.js";
@@ -20,6 +21,7 @@ app.set("view engine", "ejs");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // API Routes
+app.use("/", UserController.getAllUsers.bind(UserController));
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/accounts", accountRoutes);
